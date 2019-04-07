34ea13880a7555f3599584a6e4fc12773c3284ad
abbas mirza <hasanabbas21@gmail.com>
2019-04-06 23:04:27 -0400

    changed device type description

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -63,15 +63,18 @@ public class Application {
   
              e.printStackTrace();
          }
-
-    	String html = "<!DOCTYPE html>\n" + 
+    	
+    	String s2 = "<!DOCTYPE html>\n" + 
     			"<html>\n" + 
     			"<head>\n" + 
+  
+    			"<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n" + 
+    			"<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\">\n" + 
     			"<style>\n" + 
     			"table {\n" + 
     			"  font-family: arial, sans-serif;\n" + 
     			"  border-collapse: collapse;\n" + 
-    			"  width: 40%;\n" + 
+    			"  width: 95%;\n" + 
     			"}\n" + 
     			"\n" + 
     			"td, th {\n" + 
@@ -83,66 +86,145 @@ public class Application {
     			"tr:nth-child(even) {\n" + 
     			"  background-color: #dddddd;\n" + 
     			"}\n" + 
+    			"body {\n" + 
+    			"  font-family: Arial, Helvetica, sans-serif;\n" + 
+    			"}\n" + 
+    			"\n" + 
+    			".mobile-container {\n" + 
+    			"  max-width: 480px;\n" + 
+    			"  margin: auto;\n" + 
+    			"  background-color: #107896;\n" + 
+    			"  height: 930px;\n" + 
+    			"  color: white;\n" + 
+    			"  border-radius: 10px;\n" + 
+    			"}\n" + 
+    			"\n" + 
+    			".topnav {\n" + 
+    			"  overflow: hidden;\n" + 
+    			"  background-color: #333;\n" + 
+    			"  position: relative;\n" + 
+    			"}\n" + 
+    			"\n" + 
+    			".topnav #myLinks {\n" + 
+    			"  display: none;\n" + 
+    			"}\n" + 
+    			"\n" + 
+    			".topnav a {\n" + 
+    			"  color: white;\n" + 
+    			"  padding: 14px 16px;\n" + 
+    			"  text-decoration: none;\n" + 
+    			"  font-size: 17px;\n" + 
+    			"  display: block;\n" + 
+    			"}\n" + 
+    			"\n" + 
+    			".topnav a.icon {\n" + 
+    			"  background: black;\n" + 
+    			"  display: block;\n" + 
+    			"  position: absolute;\n" + 
+    			"  right: 0;\n" + 
+    			"  top: 0;\n" + 
+    			"}\n" + 
+    			"\n" + 
+    			".topnav a:hover {\n" + 
+    			"  background-color: #ddd;\n" + 
+    			"  color: black;\n" + 
+    			"}\n" + 
+    			"\n" + 
+    			".active {\n" + 
+    			"  background-color: #4CAF50;\n" + 
+    			"  color: white;\n" + 
+    			"}\n" + 
     			"</style>\n" + 
     			"</head>\n" + 
     			"<body>\n" + 
     			"\n" + 
-    			"<h2>MEMBER ELIGIBILITY</h2>\n" + 
+    			"<!-- Simulate a smartphone / tablet -->\n" + 
+    			"<div class=\"mobile-container\">\n" + 
     			"\n" + 
+    			"<!-- Top Navigation Menu -->\n" + 
+    			"<div class=\"topnav\">\n" + 
+    			"  <a href=\"#home\" class=\"active\">ETEDX - Blue Cross Blue Shield of MA</a>\n" + 
+    			"  <div id=\"myLinks\">\n" + 
+    			"    <a href=\"#about\">About</a>\n" + 
+    			"  </div>\n" + 
+    			"  <a href=\"javascript:void(0);\" class=\"icon\" onclick=\"myFunction()\">\n" + 
+    			"    <i class=\"fa fa-bars\"></i>\n" + 
+    			"  </a>\n" + 
+    			"</div>\n" + 
+    			"\n" + 
+    			"<div style=\"padding-left:16px\">\n" + 
+    			"  <h2>PLAN SEARCH API - v2</h2>\n" + 
+    			"  <p>This api returns a list of plans that are active as of date and a list of cenceled plans in the last 2 years</p>\n" + 
     			"<table>\n" + 
     			"  <tr>\n" + 
-    			"    <th>SUBSCRIBER #</th>\n" + 
-    			"    <th>MTN123456789</th>\n" + 
+    			"    <th>PLAN NAME</th>\n" + 
+    			"    <th>PLAN TYPE</th>\n" + 
     			"  </tr>\n" + 
     			"  <tr>\n" + 
-    			"    <td>SUBSCRIBER NAME</td>\n" + 
-    			"    <td>JOHN DOE</td>\n" + 
+    			"    <td>HMO Blue NE Premier Value with Coin</td>\n" + 
+    			"    <td>MEDICAL</td>\n" + 
     			"  </tr>\n" + 
     			"  <tr>\n" + 
-    			"    <td>PLAN NAME</td>\n" + 
-    			"    <td>HMO Blue NE Deductible</td>\n" + 
+    			"    <td>HMO Blue NE Options Deductible</td>\n" + 
+    			"    <td>MEDICAL</td>\n" + 
     			"  </tr>\n" + 
     			"  <tr>\n" + 
-    			"    <td>Dr NAME</td>\n" + 
-    			"    <td>DR. TED</td>\n" + 
+    			"    <td>HMO Blue New England Enhanced Value</td>\n" + 
+    			"    <td>MEDICAL</td>\n" + 
     			"  </tr>\n" + 
     			"  <tr>\n" + 
-    			"    <td>CO PAY</td>\n" + 
-    			"    <td>$25</td>\n" + 
+    			"    <td>HMO Blue Select Deductible</td>\n" + 
+    			"    <td>MEDICAL</td>\n" + 
     			"  </tr>\n" + 
     			"  <tr>\n" + 
-    			"    <td>SUBSCRIBER DOB</td>\n" + 
-    			"    <td>1/1/1980</td>\n" + 
+    			"    <td>Preferred Blue PPO Options Deductible</td>\n" + 
+    			"    <td>MEDICAL</td>\n" + 
     			"  </tr>\n" + 
     			"  <tr>\n" + 
-    			"    <td>MAX OUT OF POCKET</td>\n" + 
-    			"    <td>$2500</td>\n" + 
+    			"    <td>Dental Blue Pediatric Essential Benefits</td>\n" + 
+    			"    <td>DENTAL</td>\n" + 
+    			"  </tr>\n" + 
+    			"  <tr>\n" + 
+    			"    <td>Dental Blue Pediatric Essential Benefits</td>\n" + 
+    			"    <td>DENTAL</td>\n" + 
+    			"  </tr>\n" + 
+    			"  <tr>\n" + 
+    			"    <td>Dental Blue Pediatric + Adult 2</td>\n" + 
+    			"    <td>DENTAL</td>\n" + 
+    			"  </tr>\n" + 
+    			"  <tr>\n" + 
+    			"    <td>Dental Blue Pediatric + Adult Preventive</td>\n" + 
+    			"    <td>DENTAL</td>\n" + 
     			"  </tr>\n" + 
     			"</table>\n" + 
     			"\n" + 
     			"\n" + 
-    			"<h2>ACCUMULATIONS</h2>\n" + 
+    			"<h2>CANCELED PLANS</h2>\n" + 
     			"\n" + 
-    			"<table>\n" + 
+				"<table>\n" + 
     			"  <tr>\n" + 
-    			"    <th>SUBSCRIBER #</th>\n" + 
-    			"    <th>MTN123456789</th>\n" + 
+    			"    <th>PLAN NAME</th>\n" + 
+    			"    <th>CANCEL DATE</th>\n" + 
     			"  </tr>\n" + 
     			"  <tr>\n" + 
-    			"    <td>INDIVIDUAL DEDUCTIBLE MET </td>\n" + 
-    			"    <td>$700</td>\n" + 
+    			"    <td>Blue Choice Plan 2 Value Plus</td>\n" + 
+    			"    <td>2018-12-31</td>\n" + 
     			"  </tr>\n" + 
     			"  <tr>\n" + 
-    			"    <td>FAMILY DEDUCTIBLE MET</td>\n" + 
-    			"    <td>$2000</td>\n" + 
+    			"    <td>HMO Blue Enhanced Value</td>\n" + 
+    			"    <td>2018-12-31</td>\n" + 
     			"  </tr>\n" + 
     			"  <tr>\n" + 
-    			"    <td>PHYSIO VISITS LEFT</td>\n" + 
-    			"    <td>6/10</td>\n" + 
+    			"    <td>PPO Student Health Plan</td>\n" + 
+    			"    <td>2017-05-31</td>\n" + 
+    			"  </tr>\n" + 
+    			"  <tr>\n" + 
+    			"    <td>Network Blue Premier Value</td>\n" + 
+    			"    <td>2018-12-31</td>\n" + 
     			"  </tr>\n" + 
     			"</table>\n" + 
-				"\n" + 
-				"\n" + 
+    			"\n" + 
+    			"\n" + 
     			"<h2>ANALYTICS</h2>\n" + 
     			"\n" + 
 				"<table>\n" + 
@@ -151,14 +233,29 @@ public class Application {
     			"    <th>"+ deviceType +"</th>\n" + 
     			"  </tr>\n" + 
 				"  <tr>\n" + 
-				"    <td>SERVICE CALL COUNTER </td>\n" + 
+				"    <td>NUMBER OF REQUESTS</td>\n" + 
 				"    <td>"+counter+"</td>\n" + 
 				"  </tr>\n" + 
-				"</table>\n" + 
+    			"</div>\n" + 
+    			"\n" + 
+    			"<!-- End smartphone / tablet look -->\n" + 
+    			"</div>\n" + 
+    			"\n" + 
+    			"<script>\n" + 
+    			"function myFunction() {\n" + 
+    			"  var x = document.getElementById(\"myLinks\");\n" + 
+    			"  if (x.style.display === \"block\") {\n" + 
+    			"    x.style.display = \"none\";\n" + 
+    			"  } else {\n" + 
+    			"    x.style.display = \"block\";\n" + 
+    			"  }\n" + 
+    			"}\n" + 
+    			"</script>\n" + 
+    			"\n" + 
     			"</body>\n" + 
     			"</html>";
        
-    	return html;
+    	return s2;
     }
  
 	public static void main(String[] args) {
